5450654136d9ad6fb1045913fbe6fb0678ae2e1b
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2018-01-15 17:13:48 +0900

    feat: initial commit

---
+++ Makefile
@@ -0,0 +1,36 @@
+PROJECT := project
+NAME := name
+VERSION := 0.0.0
+REVISION := $(shell git rev-parse --short HEAD)
+
+SRCS := $(shell find . -type f -name '*.go')
+LDFLAGS := -w -X 'main.Version=$(VERSION)' -X 'main.Revision=$(REVISION)'
+
+build: $(SRCS) vendor
+	go build -ldflags="$(LDFLAGS)" -o bin/$(NAME)
+
+static-build: $(SRCS)
+	go build -a -tags netgo -installsuffix netgo -ldflags="$(LDFLAGS) -extldflags '-static'" -o bin/$(NAME)
+
+docker-build: Dockerfile Gopkg.lock
+	docker build -t $(PROJECT)/$(NAME):$(VERSION) .
+
+test:
+	go test -v
+
+tag:
+	git tag $(VERSION)
+
+vendor: Gopkg.toml
+	dep ensure
+
+Gopkg.lock:
+	dep ensure
+
+Gopkg.toml:
+	dep init
+
+Dockerfile: Dockerfile.tmpl
+	NAME=$(NAME) sh $< > $@
+
+.PHONEY: build static-build docker-build test tag
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	Version  string
+	Revision string
+)
+
+type key int
+
+const (
+	requestIDKey key = 0
+)
+
+var (
+	listenAddr string
+	healthy    int32
+)
+
+func main() {
+	flag.StringVar(&listenAddr, "l", ":5000", "server listen address")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	logger.Println("server is starting...")
+	logger.Printf("version:  %s", Version)
+	logger.Printf("revision: %s", Revision)
+
+	router := http.NewServeMux()
+	router.Handle("/healthz", healthz())
+
+	nextRequestID := func() string {
+		return strconv.FormatInt(time.Now().UnixNano(), 10)
+	}
+
+	server := &http.Server{
+		Addr:         listenAddr,
+		Handler:      tracing(nextRequestID)(logging(logger)(router)),
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	done := make(chan bool)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		logger.Println("server is shutting down...")
+		atomic.StoreInt32(&healthy, 0)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		server.SetKeepAlivesEnabled(false)
+		if err := server.Shutdown(ctx); err != nil {
+			logger.Fatalf("could not gracefully shutdown the server: %v", err)
+		}
+		close(done)
+	}()
+
+	logger.Println("server is ready to handle requests at", listenAddr)
+	atomic.StoreInt32(&healthy, 1)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("could not listen on %s: %v", listenAddr, err)
+	}
+
+	<-done
+	logger.Println("server stopped")
+}
+
+func healthz() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if atomic.LoadInt32(&healthy) == 1 {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		w.WriteHeader(http.StatusServiceUnavailable)
+	})
+}
+
+func logging(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			defer func() {
+				requestID, ok := req.Context().Value(requestIDKey).(string)
+				if !ok {
+					requestID = "unknown"
+				}
+				logger.Println(requestID, req.Method, req.URL.Path, req.RemoteAddr, req.UserAgent())
+			}()
+			next.ServeHTTP(w, req)
+		})
+	}
+}
+
+func tracing(nextRequestID func() string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			requestID := req.Header.Get("Request-Id")
+			if requestID == "" {
+				requestID = nextRequestID()
+			}
+			ctx := context.WithValue(req.Context(), requestIDKey, requestID)
+			w.Header().Set("Request-Id", requestID)
+			next.ServeHTTP(w, req.WithContext(ctx))
+		})
+	}
+}
